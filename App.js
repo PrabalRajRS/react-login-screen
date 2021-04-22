85e83d34ade157ead22e2dfcfe50ae29b71e6703
Prabalraj23 <63351991+Prabalraj23@users.noreply.github.com>
2021-04-22 13:53:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './home/home'
+import 'antd/dist/antd.css';
+import StudentRegistration from './student-registration/student-registration';
+import StudentsRegistration from './student-registration/studentsRegistration';
+
+function App() {
+  return (
+    <div className="App">
+    {/*<Home />*/}
+     {/*<DetailsPage />*/}
+     {/* <Login /> */}
+     
+     {/* <StudentRegistration /> */}
+     <StudentsRegistration />
+    </div>
+  );
+}
+
+export default App;
